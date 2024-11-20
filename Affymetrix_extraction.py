8db75f561fd7730ad090d36848c91ed0ff5408e6
william <william_walker@gmx.com>
2024-11-20 03:32:44 -0600

    more MI

--- Affymetrix_extraction.py
+++ Affymetrix_extraction.py
@@ -34,6 +34,9 @@ sample_info_df = pd.read_csv(repopath+"auxiliary_files/samples.csv")
 for title, accession in zip(sample_info_df["Title"], sample_info_df["Accession"]):
     cancer_type = title.split(",")[1].split("-")[0].strip()
     chpfilepath_dict[cancer_type].append(get_filepath_from_accession(accession, chpfilepath_list))
+del title
+del accession
+gc.collect()
 # %% load in the probeset tsv data into a dataframe
 os.chdir(repopath)
 probeset_df = pd.read_csv("./auxiliary_files/GPL570_probeset.tsv", sep="\t")
@@ -49,13 +52,13 @@ control_probes = probeset_df[probeset_df["SPOT_ID"] == '--Control']["ID"]
 #         ctrl_vals = df.loc[control_probes]
 #         break
 #     break
-# %% Loads in the data and targets for the classifier from the raw dataframes
+# %% Loads in the data and targets for the classifier from the raw dataframes and sticks them in two dicts;
+# one for the targets and one for the sample information (whether transcript was absent, present etc. )
 
 signal_list = []
 apm_list = []  # absent, present, missing list
 target_list = []
 os.chdir(repopath+"chp_extracted/")
-signal_dict = defaultdict(list)
 apm_dict = defaultdict(list)
 target_dict = defaultdict(list)
 
@@ -76,10 +79,13 @@ for key in tqdm(chpfilepath_dict.keys()):
 # signal_array = np.array(signal_list)
 # apm_array = np.array(apm_list)
 
+del df
 del apm_list
 del signal_list
 gc.collect()
-# %% Mutual information
+
+
+# %% sub sample the data into two categories e.g. "control" and "bladder cancer"
 import copy
 
 def subsample_data(apm_dict, targ_dict, key1, key2 = "control"):
@@ -93,33 +99,41 @@ def subsample_data(apm_dict, targ_dict, key1, key2 = "control"):
 
 sub_apm, sub_targets = subsample_data(apm_dict, target_dict, "colorectal cancer", "control")
 
-def get_mi_data(key1, apm_dict, targ_dict, key2 = "control"):
-    data =  copy.copy(apm_dict[key1])
-    data.extend(apm_dict[key2])
-    data = np.array(data)
-    print(data.shape)
-    targs = copy.copy(targ_dict[key1])
-    targs.extend(targ_dict[key2])
-    targs = np.array(targs)
+
+# %% Get Mutual information
+def get_mi_data(data, targs):
     probe_mi_list = []
-    for i in tqdm(range(data.shape[1])):
+    for i in tqdm(range(data.shape[1]), leave=False):
         probe_mi_list.append(metrics.mutual_info_score(data[:, i], targs))
     return probe_mi_list
 
-for key in apm_dict.keys():
+probe_mi_dict = defaultdict(list)
+for key in tqdm(apm_dict.keys()):
     if key != "control":
-        probe_mi_list = get_mi_data("colorectal cancer", apm_dict, target_dict)
-        break
-# %%
-ranked_probe_importance_indices = np.argsort(probe_mi_list, )[::-1]
-ranked_probe_importances = np.sort(probe_mi_list, )[::-1]
-most_important_probes = df["Probe Set Name"].iloc[ranked_probe_importance_indices[:20]].to_numpy()
-ranked_probe_importances = ranked_probe_importances[:20]
-for probe, importance in zip(most_important_probes, ranked_probe_importances):
-    psdf = probeset_df.loc[probeset_df["ID"] == probe]
-    print("Importance:  " + str(importance))
-    print(psdf["Gene Title"].to_numpy())
-    print()
+        sub_apm, sub_targets = subsample_data(apm_dict, target_dict, key, "control")
+        probe_mi_dict[key] = get_mi_data(sub_apm, sub_targets)
+        
+# %% Print interesting things about mutual information
+
+num_most_important = 10
+
+for key in tqdm(probe_mi_dict.keys()):
+    ranked_probe_importance_indices = np.argsort(probe_mi_dict[key] )[::-1]
+    ranked_probe_importances = np.sort(probe_mi_dict[key] )[::-1]
+    most_important_probes = df["Probe Set Name"].iloc[ranked_probe_importance_indices[:2000]].to_numpy()
+    ranked_probe_importances = ranked_probe_importances[:2000]
+    
+    i = 0
+    for  probe, importance in zip(most_important_probes, ranked_probe_importances):
+        psdf = probeset_df.loc[probeset_df["ID"] == probe]
+        if psdf["Gene Symbol"] != np.nan:
+            print("Importance:  " + str(importance))
+            print(psdf["Gene Symbol"].to_numpy()[0])
+            print(psdf["ENTREZ_GENE_ID"].to_numpy()[0])
+            print()
+            i+=1
+        if i == num_most_important:
+            break
 # %% organizes the data into training and test
 # TODO: add shuffling, cross validation
 import random
